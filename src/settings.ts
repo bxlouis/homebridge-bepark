7c88f3838c2ed432e7b73d0bacdf5720a619cc14
bxlouis <bxlouis@users.noreply.github.com>
2023-10-11 23:22:36 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeBePark';
+export const PLATFORM_NAME = 'BeParkGarageDoor';
 
 /**
  * This must match the name of your plugin as defined the package.json
